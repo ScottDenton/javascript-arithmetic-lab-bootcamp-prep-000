db36690d713e0f5cc92c134c5a3d9f69cd1d857e
scottyd_14@hotmail.com <scottyd_14@hotmail.com>
2018-09-09 19:24:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,23 +1,18 @@
 
 
 
-function add(a, b) {
+function add (a, b) {
   return a + b;
 }
 
+function subtract (a, b) {
+  return a - b;
+}
 
-it('add(a, b) adds two numbers and returns the result', function() {
-  expect(add(a, b)).toEqual(a + b)
-})
+function multiply (a, b) {
+  return a * b;
+}
 
-it('subtract(a, b) subtracts b from a and returns the result', function() {
-  expect(subtract(a, b)).toEqual(a - b)
-})
-
-it('multiply(a, b) multiplies two numbers and returns the result', function() {
-  expect(multiply(a, b)).toEqual(a * b)
-})
-
-it('divide(a, b) divides a by b and returns the result', function() {
-  expect(divide(a, b)).toEqual(a / b)
-})
+function divide (a, b) {
+  return a / b;
+}
